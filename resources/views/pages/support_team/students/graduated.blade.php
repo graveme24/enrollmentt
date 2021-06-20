f54bbee7847db0c834e2794438367a8c2a5b11fc
graveme24 <vallejo.adrian25@gmail.com>
2021-06-20 15:13:21 +0800

    commit on heroku

--- resources/views/pages/support_team/students/graduated.blade.php
+++ resources/views/pages/support_team/students/graduated.blade.php
@@ -39,7 +39,7 @@
                     @foreach($students as $s)
                     <tr>
                         <td>{{ $loop->iteration }}</td>
-                        <td><img class="rounded-circle" style="height: 40px; width: 40px;" src="{{ asset('/storage/'.config('chatify.user_avatar.folder').'/'.$s->user->avatar) }}" alt="photo"></td>
+                        <td><img class="rounded-circle" style="height: 40px; width: 40px;" src="{{ Storage::disk('s3')->url('public/avatar/' . $s->user->avatar) }}" alt="photo"></td>
                         <td>{{ $s->user->name }}</td>
                         <td>{{ $s->adm_no }}</td>
                         <td>{{ $s->my_class->name.' '.$s->section->name }}</td>
